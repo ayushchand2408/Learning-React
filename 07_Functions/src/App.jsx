40c3cfccef62b7c0f9b1abc8ab60088807ccd602
Ayush Chand <ayushchand862@gmail.com>
2026-06-15 16:34:20 +0530

    Learned how to call functions in react ans how to use elem object effficiently

---
+++ 07_Functions/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function App(){
+  
+  // const btnClicked = () => {
+  //   console.log(`button is clicked  times`);
+  // <button onClick={function(){
+  //       console.log("Button Clicked");
+  //     }} > Sumbit</button>
+  // }
+  // function display(val){
+  //   console.log(val);
+  // }
+  // <div className="">
+  //     <h1>Hello , Ayush</h1>
+  //     <input onChange={function(elems){
+  //       display(elems.target.value);
+  //     }} type="text" placeholder="Enter yor name"></input>
+  //   </div>
+  return(
+    <hi>byy</hi>
+  );
+}
+
+export default App;
