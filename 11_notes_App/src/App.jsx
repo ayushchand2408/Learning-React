bda7d74905206e9688e04fb32644da5f8d3109c5
Ayush Chand <ayushchand862@gmail.com>
2026-06-19 17:43:23 +0530

    create new project my-notes with only react

--- 11_notes_App/src/App.jsx
+++ 11_notes_App/src/App.jsx
@@ -1,14 +1,33 @@
 import React, { useState } from "react";
+import { X } from 'lucide-react';
 import './App.css'
 
 const App = () => {
   const[title , setTitle] = useState("");
   const[details , setDetails] = useState("");
+  const[task , setTask] = useState([]);
+
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(title);
-    console.log(details)
+    const copyTask = [...task];
+    copyTask.push({title,details});
+
+    setTask(copyTask);
+    console.log(task);
+
+
+    setDetails('');
+    setTitle('');
+  }
+
+  const deleteNote = (idx) => {
+    const copyTask = [...task];
+
+    copyTask.splice(idx,1);
+
+    setTask(copyTask);
+
   }
 
   return (
@@ -30,15 +49,24 @@ const App = () => {
           value={details} 
           className="px-5 w-full font-medium h-32 py-2 flex-row py-2 h-20 outline-none  border-2 rounded "
           />
+
           <button 
-          className="bg-white w-full text-black px-5 py-2 outline-none rounded ">Add Note</button>        
+          className="bg-white active:scale-95 w-full text-black px-5 py-2 outline-none rounded ">
+            Add Note
+          </button>        
       </form>
       <div className="lg:w-1/2  bg-gray-900  p-10">
         <h1 className="text-3xl font-bold" >Saved Notes</h1>
-        <div className="flex h-full flex-wrap gap-5 mt-3 overflow-auto">
-          <div className="h-52 w-40 rounded-2xl bg-white"></div>
-          <div className="h-52 w-40 rounded-2xl bg-white"></div>
-          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+        <div className="flex h-[90%] flex-wrap gap-5 mt-3 overflow-auto">
+          {task.map(function(elem , idx){
+            return <div key={idx} className="relative h-52 w-40 rounded-2xl bg-cover text-black overflow-hidden bg-white p-4 bg-[url('https://cdn5.vectorstock.com/i/1000x1000/20/29/yellow-sticky-note-template-taped-office-memo-vector-46412029.jpg')]">
+              <h2 onClick={(e)=>{
+                deleteNote(idx);
+              }}className="absolute top-5 right-5 bg-red-500 p-1 rounded-full text-xs"><X /></h2>
+              <h3 className="mt-2 leading-tight font-bold">{elem.title}</h3>
+              <p className="mt-2 leading-tight font-medium">{elem.details}</p>
+             </div> 
+          })}
           
         </div>
       </div>
