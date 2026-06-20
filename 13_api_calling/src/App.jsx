f6a88c61c5879a41b74347484483704164e14d5b
Ayush Chand <ayushchand862@gmail.com>
2026-06-20 21:07:10 +0530

    understand axios library

--- 13_api_calling/src/App.jsx
+++ 13_api_calling/src/App.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import axios from 'axios';
 
 const App = () =>{
 
-  async function getData(){
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1') // wait till the api give respnse 
-    const data = await response.json()
+  // async function getData(){
+  //   // const response = await fetch('https://jsonplaceholder.typicode.com/todos/1') // wait till the api give respnse 
+  //   // const data = await response.json()
 
-    console.log(data)
+  //   // console.log(data)
+
+  // }
+
+  // const getData = async () => {
+  //   // const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  //   const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/1');//destructering
+  //   // console.log(response.data)
+  //   console.log(data);
+  // }
+  
+  const getData = async () => {
+    // const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/1');//destructering
+    // console.log(response.data)
+    console.log(data);
   }
   return (
     <div className="">
